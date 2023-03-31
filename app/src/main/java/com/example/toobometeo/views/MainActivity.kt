dbed26dfef33a9c977f4988bf96e5f166b067be7
VictorNail <victor.laine@free.fr>
2023-03-31 16:23:22 +0200

    rajout icon + bug resolv number

--- app/src/main/java/com/example/toobometeo/views/MainActivity.kt
+++ app/src/main/java/com/example/toobometeo/views/MainActivity.kt
@@ -67,6 +67,9 @@ class MainActivity : AppCompatActivity() {
         applicationScope.launch(Dispatchers.IO) { respository.insert(WeatherCityEntity("Test")) }
 
 
+        val searchDelay = 500L
+        val searchScope = CoroutineScope(Dispatchers.Main)
+
         val searchBar = findViewById<EditText>(R.id.search_bar)
         val cityApi = localisationHelper.getInstance().create(adressGouvApi::class.java)
         searchBar.addTextChangedListener(object : TextWatcher {
@@ -80,58 +83,83 @@ class MainActivity : AppCompatActivity() {
 
             override fun afterTextChanged(s: Editable?) {
                 // launching a new coroutine
-                GlobalScope.launch {
-                    val result = cityApi.getSearch(searchBar.text.toString())
-                    runOnUiThread {
-                        textViewCity.text = result.body()?.features?.get(0)?.properties?.label ?: ""
-                        val latitude:Double = result.body()?.features?.get(0)?.geometry?.coordinates?.get(1) ?: 44.8378
-                        val longitude:Double = result.body()?.features?.get(0)?.geometry?.coordinates?.get(0) ?: 0.5792
-                        GlobalScope.launch {
-                            val result = weatherApi.getForecast(
-                                latitude,
-                                longitude,
-                                "temperature_2m,weathercode,windspeed_180m",
-                                "weathercode",
-                                "Europe/London"
-                            )
-                            val temperatures = result.body()?.hourly?.temperature_2m
-                            if (temperatures != null) {
+                searchScope.launch {
+                    delay(searchDelay)
+                    if (searchBar.text.toString().isNotBlank()) {
+                        runOnUiThread {
+                            GlobalScope.launch {
+                                val result = cityApi.getSearch(searchBar.text.toString())
                                 runOnUiThread {
-                                    textViewDegre.text = temperatures[hour].toString() + "°C"
-                                }
-                            }
-                            val codes = result.body()?.hourly?.weathercode
-                            //changement background
-                            runOnUiThread {
-                                if (linearLayout != null) {
-                                    when (codes?.get(hour)?.toInt()) {
-                                        0 -> linearLayout.setBackgroundResource(R.drawable.soleil)
-                                        1, 2, 3 -> linearLayout.setBackgroundResource(R.drawable.eclaircie)
-                                        85, 86, 71, 73, 75, 77 -> linearLayout.setBackgroundResource(R.drawable.snow)
-                                        80, 81, 82 -> linearLayout.setBackgroundResource(R.drawable.pluie)
-                                        45, 48 -> linearLayout.setBackgroundResource(R.drawable.fog)
-                                        95, 96, 99 -> linearLayout.setBackgroundResource(R.drawable.eclair)
-                                        51, 53, 55, 56, 57, 61, 63, 65, 66, 67 -> linearLayout.setBackgroundResource(R.drawable.petitepluie)
-                                        else -> {
-                                            linearLayout.setBackgroundResource(R.drawable.soleil)
+                                    textViewCity.text =
+                                        result.body()?.features?.get(0)?.properties?.label ?: ""
+                                    val latitude: Double =
+                                        result.body()?.features?.get(0)?.geometry?.coordinates?.get(
+                                            1
+                                        )
+                                            ?: 44.8378
+                                    val longitude: Double =
+                                        result.body()?.features?.get(0)?.geometry?.coordinates?.get(
+                                            0
+                                        )
+                                            ?: 0.5792
+                                    GlobalScope.launch {
+                                        val result = weatherApi.getForecast(
+                                            latitude,
+                                            longitude,
+                                            "temperature_2m,weathercode,windspeed_180m",
+                                            "weathercode",
+                                            "Europe/London"
+                                        )
+                                        val temperatures = result.body()?.hourly?.temperature_2m
+                                        if (temperatures != null) {
+                                            runOnUiThread {
+                                                textViewDegre.text =
+                                                    temperatures[hour].toString() + "°C"
+                                            }
+                                        }
+                                        val codes = result.body()?.hourly?.weathercode
+                                        //changement background
+                                        runOnUiThread {
+                                            if (linearLayout != null) {
+                                                when (codes?.get(hour)?.toInt()) {
+                                                    0 -> linearLayout.setBackgroundResource(R.drawable.soleil)
+                                                    1, 2, 3 -> linearLayout.setBackgroundResource(R.drawable.eclaircie)
+                                                    85, 86, 71, 73, 75, 77 -> linearLayout.setBackgroundResource(
+                                                        R.drawable.snow
+                                                    )
+                                                    80, 81, 82 -> linearLayout.setBackgroundResource(
+                                                        R.drawable.pluie
+                                                    )
+                                                    45, 48 -> linearLayout.setBackgroundResource(R.drawable.fog)
+                                                    95, 96, 99 -> linearLayout.setBackgroundResource(
+                                                        R.drawable.eclair
+                                                    )
+                                                    51, 53, 55, 56, 57, 61, 63, 65, 66, 67 -> linearLayout.setBackgroundResource(
+                                                        R.drawable.petitepluie
+                                                    )
+                                                    else -> {
+                                                        linearLayout.setBackgroundResource(R.drawable.soleil)
+                                                    }
+                                                }
+                                            } else {
+                                                Log.d("MainActivity", "Layout not shown yet")
+                                            }
+                                        }
+                                        //Prochain Changement
+                                        var i = hour
+                                        var stop = true
+                                        codes?.forEach { code ->
+                                            i++
+                                            if (codes?.get(hour) != code && stop) {
+                                                var hourChange = i - hour
+                                                runOnUiThread {
+                                                    textChange.text =
+                                                        "Prochain changement dans: " + hourChange.toString() + "H"
+                                                }
+                                                stop = false
+                                            }
                                         }
                                     }
-                                } else {
-                                    Log.d("MainActivity", "Layout not shown yet")
-                                }
-                            }
-                            //Prochain Changement
-                            var i = hour
-                            var stop =true
-                            codes?.forEach { code ->
-                                i++
-                                if(codes?.get(hour) != code && stop){
-                                    var hourChange = i-hour
-                                    runOnUiThread {
-                                        textChange.text =
-                                            "Prochain changement dans: " + hourChange.toString() +"H"
-                                    }
-                                    stop = false
                                 }
                             }
                         }
